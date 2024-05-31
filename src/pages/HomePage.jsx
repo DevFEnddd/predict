7f83f30e9c5529e15907d7d05be3efbf315ba033
MinWanggg <minwanggg@Mac-mini-cua-MinWanggg.local>
2024-05-31 12:03:35 +0700

    delete line

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,7 +23,6 @@ const HomePage = () => {
         cleaned_job: job,
         cleaned_title: title,
       }),
-      referrerPolicy: "unsafe-url" 
     })
       .then((response) => {
         if (!response.ok) {
