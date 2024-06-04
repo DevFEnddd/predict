a4ca2204f87cbee1ebe7608fbf0353c1ecad8b24
MinWanggg <minwanggg@Mac-mini-cua-MinWanggg.local>
2024-06-04 13:41:39 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.1.214',
+    port: 5173, // Choose your desired port
+  },
 })
